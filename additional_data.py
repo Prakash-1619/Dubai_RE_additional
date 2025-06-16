0ca3146ef03ca6568251c28c5fe8519272bd130b
Prakash-1619 <poojariprakash88@gmail.com>
2025-06-16 19:30:50 +0530

    add changes

--- additional_data.py
+++ additional_data.py
@@ -45,9 +45,11 @@ elif main_tab == "Charts":
     st.subheader("📈 Chart Visualization")
 
     sheet = st.sidebar.selectbox("Select Data file", sheet_names_main, key="chart_sheet")
-    df = pd.read_excel(excel_file_path, sheet_name=sheet)
     if sheet == 'Avg_Construction_Material_Price':
+        df = pd.read_excel(excel_file_path, sheet_name=sheet)
         df.rename(columns = {'yearen':'year'})
+    else:
+        df = pd.read_excel(excel_file_path, sheet_name=sheet)
 
     # Identify column types
     categorical_columns = df.select_dtypes(include=['object', 'string']).columns.tolist()
@@ -63,7 +65,7 @@ elif main_tab == "Charts":
             id = ['id','i_d','year']
             value_col = st.sidebar.selectbox("Select Numeric Column (Y-Axis)", [col for col in numeric_columns if col not in id], key="line_y")
             category_col = st.sidebar.selectbox("Select Category Column (Legend)", categorical_columns , key="line_legend")
-            df['year'] = df.groupby(['year',category_col])[value_col].mean()
+            df = df.groupby(['year',category_col])[value_col].mean().reset_index()
 
             fig = px.line(
                 df.dropna(subset=["year", value_col, category_col]),
